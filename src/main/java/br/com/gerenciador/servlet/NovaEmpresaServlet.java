0421ed050152281c9382211e89fc1f9ea28cded3
Noemi <noemi.jacin@gmail.com>
2022-03-21 15:38:42 -0300

    usando jsp

--- src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,11 +32,16 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa  " + nomeEmpresa + " cadastrada com sucesso!<body><html>");
+//		PrintWriter out = response.getWriter();
+//		out.println("<html><body>Empresa  " + nomeEmpresa + " cadastrada com sucesso!<body><html>");
+		
+		
+		// Chamar JSP --> parte html
+		RequestDispatcher reqdisp = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		reqdisp.forward(request, response);
 		
 	}
 	
-	
-
+ 
 }
