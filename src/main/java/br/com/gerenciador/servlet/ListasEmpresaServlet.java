1d9bbb777533fb2ceb94513fa5110fb3009e9fbd
Noemi <noemi.jacin@gmail.com>
2022-03-21 20:21:15 -0300

    Refatorando com JSP

--- src/main/java/br/com/gerenciador/servlet/ListasEmpresaServlet.java
+++ src/main/java/br/com/gerenciador/servlet/ListasEmpresaServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,17 +34,22 @@ public class ListasEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
+		request.setAttribute("empresa", lista);
 		
-		//Resposta html
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<ul>");
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		out.println("<ul>");
-		out.println("<body><html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);	
 		
+//		Refatoraso com jsp
+//		//Resposta html
+//		PrintWriter out = response.getWriter();
+//		out.println("<html><body>");
+//		out.println("<ul>");
+//		for (Empresa empresa : lista) {
+//			out.println("<li>" + empresa.getNome() + "</li>");
+//		}
+//		out.println("<ul>");
+//		out.println("<body><html>");
+//		
 	}
 
 }
