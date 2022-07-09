3b25ea5604da98e05a94156fefd613f52d9f5160
Noemi <noemi.jacin@gmail.com>
2022-07-09 14:26:58 -0300

    Servico com xml

--- src/main/java/br/com/gerenciador/servlet/EmpresasService.java
+++ src/main/java/br/com/gerenciador/servlet/EmpresasService.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.gerenciador.modelo.Banco;
 import br.com.gerenciador.modelo.Empresa;
@@ -21,15 +22,30 @@ import br.com.gerenciador.modelo.Empresa;
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
+	//	Aplicaç~çao de servico com Json
+//		
+//		Gson gson = new Gson();
+//		String json = gson.toJson(empresas);
+//		
+//		response.setContentType("application/json");
+//		response.getWriter().print(json);
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+	
+		XStream xstream = new XStream();
+		xstream.alias("empresa", Empresa.class);
+		String xml = xstream.toXML(empresas); 
+
+		
+		response.setContentType("application/xml");
+		response.getWriter().print(xml);
+
+		
+
 	}
 
 }
